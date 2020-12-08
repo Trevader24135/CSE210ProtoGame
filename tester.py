8036e923970c026b8b274c5e983c6fd3ffc23498
Trevader24135 <trklingler3@gmail.com>
2020-12-07 20:02:40 -0700

    Updated map
    +added more enemies
    +expanded map and end room
    +disabled AI map debug
    + slightly improved (SLIGHTLY) ai pathing
    + AI now only path if they have LoS

--- tester.py
+++ tester.py
@@ -1,32 +1,3 @@
-gw = [[109,109,109],[109,109,109],[109,109,109],[109,109,109]]
-gwchr = [[110,109,109],[110,109,109],[110,109,109],[110,109,109]]
-rb = [[255,0,0],[0,255,0],[255,255,255],[0,0,255]]
-gray = [[122,122,122],[122,122,122],[122,122,122],[122,122,122]]
-
-map = [
-    [gw, gw, gw, gw, gw, gw, gw],
-    [gw, 0, gw, 0, 0, 0, gw], 
-    [gw, 0, gray, 0, rb, 0, gw], 
-    [gw, 0, 0, 0, 0, 0, gw], 
-    [gwchr, 0, 0, 0, 0, 0, gw], 
-    [gw, gw, gw, 0, 0, gw, gw], 
-    [gw, gw, gw, gw, 0, gw, gw], 
-    [gw, 0, 0, 0, 0, 0, gw], 
-    [gw, gw, gw, gw, 0, gw, gw], 
-    [gw, 0, 0, 0, 0, 0, gw], 
-    [gw, 0, 0, 0, -1, 0, gw], 
-    [gw, 0, 0, 0, 0, 0, gw], 
-    [gw, gw, gw, gw, gw, gw, gw]]
-
-mapEnlargened = []
-for y in map:
-    row = []
-    for x in y:
-        if type(x) == int:
-            row.append(0)
-        else:
-            row.append(1)
-    mapEnlargened.append(row)
-
-for i in mapEnlargened:
-    print(i)
+test = [[1,2,3],[4,5,6]]
+test = [[0 for i in range(len(test) + 1)]] + test
+print(test)
